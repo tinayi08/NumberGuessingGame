1f9e5e5d98b2f6fb3607160750d2d2ca76f9253b
tinayi08 <christinayi827@gmail.com>
2021-10-27 15:50:04 -0700

    update GUI

--- src/GUI.java
+++ src/GUI.java
@@ -7,6 +7,7 @@ public class GUI {
     private static JLabel label;
     private static JPanel panel;
     private Display display;
+    private static GridBagConstraints c;
 
     public GUI() {
         display = new Display();
@@ -16,28 +17,58 @@ public class GUI {
         GUI start = new GUI();
 
         frame = new JFrame();
-
-        panel = new JPanel();
-        frame.setSize(600, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel(new GridBagLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(30,200,10,200));
+        c = new GridBagConstraints();
+        c.insets = new Insets(10,10,10,10);
         frame.add(panel);
+        frame.setSize(600, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         start.intro();
+        start.enterAGuess();
+
         frame.setVisible(true);
     }
 
     public void intro() {
         //JLabel intro = new JLabel("<html>" + display.introReturnStr() + "</html>");
 
-        JTextArea textArea = new JTextArea(20, 20);
-        textArea.setText(display.introReturnStr());
-        textArea.setWrapStyleWord(false);
-        textArea.setLineWrap(true);
-        textArea.setOpaque(false);
-        textArea.setEditable(false);
-        textArea.setFocusable(false);
+        JTextArea introText = new JTextArea();
+        introText.setText(display.introReturnStr());
+        introText.setWrapStyleWord(true);
+        introText.setLineWrap(true);
+        introText.setColumns(1000);
+        introText.setOpaque(false);
+        introText.setEditable(false);
+        introText.setFocusable(false);
 
-        panel.add(textArea, BorderLayout.CENTER);
+        //panel.add(introText, BorderLayout.CENTER);
 
+        c.gridx = 0;
+        c.gridy = 1;
+        panel.add(introText, c);
+
+
+    }
+
+    public void enterAGuess() {
+
+        JTextArea promptText = new JTextArea();
+        promptText.setText("Please enter a guess:");
+        promptText.setWrapStyleWord(true);
+        promptText.setLineWrap(true);
+        promptText.setOpaque(false);
+        promptText.setEditable(false);
+        promptText.setFocusable(false);
+        panel.add(promptText);
+        JButton guess = new JButton("Guess");
+
+        c.gridx = 0;
+        c.gridy = 2;
+        panel.add(promptText,c);
+        c.gridx = 0;
+        c.gridy = 3;
+        panel.add(guess,c);
 
     }
 
